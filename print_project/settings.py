0772b1e1f0197b687cf4b46ee3f26659d6ce21fd
vikash8507 <71246736+vikash8507@users.noreply.github.com>
2021-09-19 12:55:48 +0530

    Update settings.py

--- print_project/settings.py
+++ print_project/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', 'media').replace('\\','/')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
