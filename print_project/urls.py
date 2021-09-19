ca58011c64021a2e0fd1bc43198dab7e61b24d6f
vikash8507 <71246736+vikash8507@users.noreply.github.com>
2021-09-19 12:44:57 +0530

    Update urls.py

--- print_project/urls.py
+++ print_project/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include('main.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
