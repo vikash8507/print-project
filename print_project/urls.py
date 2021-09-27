311528a33d94542d7b63b4b961a562a7d6b4ab45
vikash8507 <vikash1998bscc@gmail.com>
2021-09-27 21:04:48 +0530

    Some Fixes

--- print_project/urls.py
+++ print_project/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from users.views import register_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", register_view, name="register"),
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+from .forms import NewUserForm
+
+def register_view(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("login")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request, "registration/register.html", {"form":form})
