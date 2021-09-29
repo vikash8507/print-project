d12d46825d0c3e27492b3b473049424c8a91b98e
vikash8507 <vikash1998bscc@gmail.com>
2021-09-29 11:26:49 +0530

    SideNavbar fixed

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -10,6 +10,8 @@
 
 
 function openNav() {
+  document.getElementById("mySidenav").classList.add("open");
+  document.getElementById("mySidenav").classList.remove("close");
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -17,7 +19,16 @@ function openNav() {
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
+  document.getElementById("mySidenav").classList.add("close");
+  document.getElementById("mySidenav").classList.remove("open");
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+document.body.addEventListener('click', () => {
+  if (document.getElementById("mySidenav").classList.contains('open')) {
+      document.getElementById("mySidenav").classList.add("close");
+      document.getElementById("mySidenav").classList.remove("open");
+      closeNav();
+  }
+}, true)
