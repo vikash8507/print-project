c7440aef51c9ea0b49236d102aa1bed5970a375e
vikash8507 <vikash1998bscc@gmail.com>
2021-09-30 10:31:20 +0530

    Password Reset Done

--- print_project/settings.py
+++ print_project/settings.py
@@ -155,8 +155,9 @@ django_heroku.settings(locals())
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com’'
-EMAIL_PORT=587
-EMAIL_HOST_USER='vikash.kumar@thinkitive.com'
-EMAIL_HOST_PASSWORD='Vikash@1998'
-EMAIL_USE_TLS=True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'vikash.kumar@thinkitive.com'
+EMAIL_HOST_PASSWORD = 'Vikash@1998'
+EMAIL_PORT = 587
+ACCOUNT_EMAIL_VERIFICATION = 'none'
